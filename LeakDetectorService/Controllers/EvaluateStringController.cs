de09eee1f4c692c6b094898a932ebe7878aec3b5
NateFrogHI <n.k.mitson@gmail.com>
2022-10-01 17:36:43 -0700

    Added pulling strings from local mongodb

--- LeakDetectorService/Controllers/EvaluateStringController.cs
+++ LeakDetectorService/Controllers/EvaluateStringController.cs
@@ -5,15 +5,37 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Diagnostics;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace LeakDetectorService.Controllers
 {
+    public struct RestrictedStringEntry
+    {
+        public string _id;
+        public string restrictedString;
+
+    }
     public class EvaluateStringController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public List<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<string> documents = new List<string>();
+            var uri = "mongodb://127.0.0.1:27017/leak-detector-db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0";
+            var client = new MongoClient(uri);
+
+            // database and collection goes here
+            var db = client.GetDatabase("leak-detector-db");
+            var coll = db.GetCollection<BsonDocument>("restrictedStrings");
+            // finid code goes here
+            var cursor = coll.AsQueryable();
+            // iterate code goes here
+            foreach (var document in cursor)
+            {
+                documents.Add(document.ToString());
+            }
+            return documents;
         }
 
         // GET api/values/5
@@ -25,9 +47,11 @@ namespace LeakDetectorService.Controllers
         // POST api/values
         public string Post([FromBody] Models.RawText value)
         {
-            String[] badwords = { "Arthas", "THatOnEwOrD", "that one sentence" };
-            Trace.WriteLine(value.ToString());
-            Utils.StringEvaluator stringEvaluator = new Utils.StringEvaluator(value.Text, badwords);
+            //String[] badwords = { "Arthas", "THatOnEwOrD", "that one sentence" };
+            Utils.Db db = new Utils.Db();
+            List<string> badwords = db.GetRestrictedStrings();
+            Debug.WriteLine(value.ToString());
+            Utils.StringEvaluator stringEvaluator = new Utils.StringEvaluator(value.Text, badwords.ToArray());
             stringEvaluator.EvaluateString();
             return stringEvaluator.Report();
         }
---
+++ LeakDetectorService/Utils/Db.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace LeakDetectorService.Utils
+{
+    public class Db
+    {
+        private string uri = "mongodb://127.0.0.1:27017/leak-detector-db?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0";
+
+        public IMongoQueryable<BsonDocument> GetCollection()
+        {
+            MongoClient client = new MongoClient(uri);
+            IMongoDatabase db = client.GetDatabase("leak-detector-db");
+            IMongoCollection<BsonDocument> coll = db.GetCollection<BsonDocument>("restrictedStrings");
+            
+            return coll.AsQueryable();
+        }
+
+        public List<string> GetRestrictedStrings()
+        {
+            List<string> restrictedStrings = new List<string>();
+            IMongoQueryable<BsonDocument> collection = GetCollection();
+            foreach (var document in collection)
+            {
+                BsonElement stringElement = document.GetElement(1);
+                restrictedStrings.Add(stringElement.Value.AsString);
+            }
+            return restrictedStrings;
+        }
+    }
+}
